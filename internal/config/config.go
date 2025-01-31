fcbb688c2ac963a69b628fe7eca5fa726b19f9fb
adanyl0v <a.danylov39@gmail.com>
2025-01-31 13:06:42 +0000

    feat: implement a config reader

---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+const (
+	EnvLocal = "local"
+	EnvDev   = "dev"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+}
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-required:"true"`
+}
+
+type Reader interface {
+	Read() (*Config, error)
+}
+
+type fileReader struct {
+	path string
+}
+
+func NewFileReader(path string) Reader {
+	return &fileReader{path: path}
+}
+
+func (f *fileReader) Read() (*Config, error) {
+	cfg := new(Config)
+	if err := cleanenv.ReadConfig(f.path, cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func DefaultFilePath() string {
+	return os.Getenv("CONFIG_FILE")
+}
+
+func ReadFile(path string) (*Config, error) {
+	cfg, err := NewFileReader(path).Read()
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func MustReadFile(path string) *Config {
+	cfg, err := ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg
+}
